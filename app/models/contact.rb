3b7861fbeebac6b91a1a52af2da0b270b60aa7a3
DaniloIS <danilosantos-pg@hotmail.com>
2022-06-01 11:48:17 -0300

    Model Phones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,22 @@
 class Contact < ApplicationRecord
-  belongs_to :kind, optional: true
 
+  # Associations
+  belongs_to :kind #, optional: true
+  has_many :phones
+
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
+  end
+
+  # def to_br
+  #   { 
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }
+  # end
   #def author
   #  "Danilo Isidoro"
   #end
@@ -16,4 +32,11 @@ class Contact < ApplicationRecord
   #    include: { kind: { only: :description }}
   #  )
   #end
+  # def hello
+  #   I18n.t('hello')
+  # end
+
+  # def i18n
+  #   I18n.default_locale
+  # end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -12,6 +12,8 @@ namespace :dev do
     end
 
     puts "Tipos de contato cadastrados com sucesso!"
+    
+    ######################
 
     puts "Cadastrando os contatos..."
     100.times do |i|
@@ -23,7 +25,18 @@ namespace :dev do
       )
     end
     puts "Contatos cadastrados com sucesso!"
+
+    ######################
+    
+    puts "Cadastrando os telefones..."
+      Contact.all.each do |contact|
+        Random.rand(5).times do |i|
+          phone = Phone.create!(number: Faker::PhoneNumber.cell_phone)
+          contact.phones << phone
+          contact.save!
+        end
+      end
+    puts "Telefones cadastrados com sucesso!"
     
   end
-
 end
